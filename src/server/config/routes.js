7cf04f4aabd9457baa664f5cbb8f070d4480e1f1
Joel Spuds <joel.01@gmail.com>
2020-11-11 10:08:11 +0000

    removing old tempaltes, controllers and routes

--- src/server/config/routes.js
+++ src/server/config/routes.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 
 import * as mainController from '../controllers/main.controller';
 import * as miscController from '../controllers/misc.controller';
-import * as recallsController from '../controllers/api/v1/recalls.controller';
+/*import * as recallsController from '../controllers/api/v1/recalls.controller';
 import * as createAccountController from '../controllers/create-account';
 import * as compareTestsController from '../controllers/compare-tests/compare-tests';
 import * as suspendTestersController from '../controllers/annual-assessment-tool/suspend-testers';
@@ -15,7 +15,7 @@ import * as vtsChangeController from '../controllers/vts-changes';
 import * as vsiDuringTestController from '../controllers/vsi-during-test';
 import * as mothData from '../controllers/mot-history-data';
 import * as recalls from '../controllers/recalls';
-import * as mothController from '../controllers/moth';
+import * as mothController from '../controllers/moth';*/
 import * as PrototypeAuth from '../middlewares/authentication';
 import { isDevelopment } from '../config/constants';
 
@@ -33,10 +33,10 @@ router.get('/robots.txt', mainController.robots);
 router.get('/', mainController.index);
 
 // API Routes
-router.post('/api/v1/recalls', recallsController.recalls);
+/*router.post('/api/v1/recalls', recallsController.recalls);*/
 
 // Create account user journey
-router.get('/prototypes/create-account', createAccountController.getRoot);
+/*router.get('/prototypes/create-account', createAccountController.getRoot);
 router.get('/prototypes/create-account/email', createAccountController.getEmail);
 router.get('/prototypes/create-account/details', createAccountController.getDetails);
 router.get('/prototypes/create-account/contact-details', createAccountController.getContactDetails);
@@ -55,29 +55,29 @@ router.post(
   createAccountController.securityQuestions,
   createAccountController.postSecurityQuestions
 );
-router.post('/prototypes/create-account/password', createAccountController.passwordValidationChecks, createAccountController.postPassword);
+router.post('/prototypes/create-account/password', createAccountController.passwordValidationChecks, createAccountController.postPassword);*/
 
 // Annual Assessment tool - suspend testers path
-router.get('/prototypes/annual-assessment-admin-tool/suspend-testers', suspendTestersController.getSuspendTesters);
-router.post('/prototypes/annual-assessment-admin-tool/suspend-testers', suspendTestersController.postSuspendTesters);
+/*router.get('/prototypes/annual-assessment-admin-tool/suspend-testers', suspendTestersController.getSuspendTesters);
+router.post('/prototypes/annual-assessment-admin-tool/suspend-testers', suspendTestersController.postSuspendTesters);*/
 
 // MOT test - add tester comments
-router.get('/prototypes/mot-test', motTestResultsController.getMOTResults);
+/*router.get('/prototypes/mot-test', motTestResultsController.getMOTResults);
 router.get('/prototypes/mot-test/comment', motTestResultsController.getAddTesterComment);
 router.post('/prototypes/mot-test/comment', motTestResultsController.postAddTesterComment);
 router.get('/prototypes/mot-test/comment/edit', motTestResultsController.getEditTesterComment);
 router.post('/prototypes/mot-test/comment/edit', motTestResultsController.postEditTesterComment);
 router.post('/prototypes/mot-test/comment/remove/', motTestResultsController.destorySession);
-router.get('/prototypes/mot-test/review', motTestResultsController.getReview);
+router.get('/prototypes/mot-test/review', motTestResultsController.getReview);*/
 
 // Speech to text - add defect path
-router.get('/prototypes/speech-to-text/', speechToTextController.getSearchQuery);
+/*router.get('/prototypes/speech-to-text/', speechToTextController.getSearchQuery);
 router.post('/prototypes/speech-to-text/categories/', speechToTextController.postSearchQuery);
 router.post('/prototypes/speech-to-text/add-major-failure/', speechToTextController.captureFormValues);
-router.get('/prototypes/speech-to-text/remove-defect', speechToTextController.removeDefect);
+router.get('/prototypes/speech-to-text/remove-defect', speechToTextController.removeDefect);*/
 
 // Site review paths
-router.get('/prototypes/site-review/', siteReviewController.clearReviewSession);
+/*router.get('/prototypes/site-review/', siteReviewController.clearReviewSession);
 router.get('/prototypes/site-review/choose-section', siteReviewController.getChooseSection);
 router.get('/prototypes/site-review/assessment/activity', siteReviewController.getAssessment);
 router.get('/prototypes/site-review/assessment/compliance', siteReviewController.getAssessment);
@@ -112,16 +112,16 @@ router.post('/prototypes/site-review/assessment/premises', [
   siteReviewController.postAssessment,
 ]);
 router.post('/prototypes/site-review/enter-details', [siteReviewController.validateDetails, siteReviewController.postDetails]);
-router.post('/prototypes/site-review/v5/assessment-activity', siteReviewController.branchOnActivity);
+router.post('/prototypes/site-review/v5/assessment-activity', siteReviewController.branchOnActivity);*/
 
 // Brake tests
-router.post('/prototypes/brake-test-config', brakeTestController.postBrakeConfig);
-router.post('/prototypes/brake-test-entry', brakeTestController.postBrakeEntry);
+/*router.post('/prototypes/brake-test-config', brakeTestController.postBrakeConfig);
+router.post('/prototypes/brake-test-entry', brakeTestController.postBrakeEntry);*/
 
 // MTS Messaging
-router.param('messageIndex', messagingController.isValidMessage);
+/*router.param('messageIndex', messagingController.isValidMessage);*/
 
-router.get('/prototypes/messages/homepage', [
+/*router.get('/prototypes/messages/homepage', [
   messagingController.setupMessages,
   messagingController.setNotificationCounts,
   messagingController.getHomepage,
@@ -136,11 +136,11 @@ router.get('/prototypes/messages/acknowledge/:messageIndex', [
 router.get('/prototypes/messages/accept/:messageIndex', messagingController.acceptMessage);
 router.get('/prototypes/messages/reject/:messageIndex', messagingController.rejectMessage);
 router.get('/prototypes/messages/archive/:messageIndex', messagingController.archiveMessage);
-router.get('/prototypes/mts-messages', messagingController.resetMessages);
+router.get('/prototypes/mts-messages', messagingController.resetMessages);*/
 
 // VTS changes
 
-router.get('/prototypes/vts-changes/', vtsChangeController.resetSession);
+/*router.get('/prototypes/vts-changes/', vtsChangeController.resetSession);
 router.get('/prototypes/vts-changes/home', vtsChangeController.startJourney);
 router.get('/prototypes/vts-changes/start', vtsChangeController.getStart);
 router.get('/prototypes/vts-changes/type', vtsChangeController.getStage);
@@ -152,13 +152,13 @@ router.get('/prototypes/vts-changes/layout', vtsChangeController.getStage);
 router.get('/prototypes/vts-changes/classes', vtsChangeController.getStage);
 router.post('/prototypes/vts-changes/classes', [vtsChangeController.validateStage, vtsChangeController.postStage]);
 router.get('/prototypes/vts-changes/summary', vtsChangeController.getStage);
-router.get('/prototypes/vts-changes/short-summary', vtsChangeController.getStage);
+router.get('/prototypes/vts-changes/short-summary', vtsChangeController.getStage);*/
 
 // Recalls
-router.post('/prototypes/recalls/V1/type-of-recall', [recalls.validateType, recalls.postRecallType]);
+/*router.post('/prototypes/recalls/V1/type-of-recall', [recalls.validateType, recalls.postRecallType]);*/
 
 // VSI during test
-router.get('/prototypes/vsi-during-test/inspection/inspection-2', vsiDuringTestController.getInspection2);
+/*router.get('/prototypes/vsi-during-test/inspection/inspection-2', vsiDuringTestController.getInspection2);
 router.get('/prototypes/vsi-during-test/inspection/inspection-3', vsiDuringTestController.getInspection3);
 router.get('/prototypes/vsi-during-test/inspection/inspection-4', vsiDuringTestController.getInspection4);
 router.get('/prototypes/vsi-during-test/inspection/inspection-5', vsiDuringTestController.getInspection5);
@@ -166,10 +166,10 @@ router.get('/prototypes/vsi-during-test/summary', vsiDuringTestController.getSum
 router.get('/prototypes/vsi-during-test/inspection', [vsiDuringTestController.initViewData, vsiDuringTestController.getInspection1]);
 router.get('/prototypes/vsi-during-test/advice', [vsiDuringTestController.getPrevUrl, vsiDuringTestController.getAdvice]);
 router.get('/prototypes/vsi-during-test/odometer', [vsiDuringTestController.getPrevUrl, vsiDuringTestController.getOdometer]);
-router.post('/prototypes/vsi-during-test/odometer', vsiDuringTestController.postOdometer);
+router.post('/prototypes/vsi-during-test/odometer', vsiDuringTestController.postOdometer);*/
 
 // MOTH Data
-router.post('/prototypes/mot-history-data/:version/enter-v5c', [mothData.checkV5c, mothData.postV5c]);
+/*router.post('/prototypes/mot-history-data/:version/enter-v5c', [mothData.checkV5c, mothData.postV5c]);
 
 router.get('/prototypes/mot-history-data/:version/history-results-audi', mothData.initViewData);
 router.get('/prototypes/mot-history-data/:version/enter-v5c', mothData.initViewData);
@@ -177,10 +177,10 @@ router.get('/prototypes/mot-history-data/:version/enter-v5c-fail', mothData.init
 
 router.get('/prototypes/mot-history-data/:version/enter-v5c-error', mothData.getError);
 router.get('/prototypes/mot-history-data/cvs-v3/history-results-audi-error1', mothData.getError);
-router.get('/prototypes/mot-history-data/cvs-v3/history-results-audi-error2', mothData.getError);
+router.get('/prototypes/mot-history-data/cvs-v3/history-results-audi-error2', mothData.getError);*/
 
 // Compare tests
-router.get('/prototypes/compare-tests/v8/start', compareTestsController.getStart);
+/*router.get('/prototypes/compare-tests/v8/start', compareTestsController.getStart);
 router.get('/prototypes/compare-tests/v8/overview', compareTestsController.getOverview);
 router.get('/prototypes/compare-tests/v8/record-outcome', compareTestsController.getRecordOutcome);
 router.get('/prototypes/compare-tests/v8/assess-difference/:defectIndex', compareTestsController.getDifference);
@@ -191,12 +191,12 @@ router.post('/prototypes/compare-tests/v8/assess-difference/:defectIndex', [
   compareTestsController.postDifference,
 ]);
 router.post('/prototypes/compare-tests/v8/record-outcome', compareTestsController.postRecordOutcome);
-router.post('/prototypes/compare-tests/v8/shortcomings', compareTestsController.postShortcomings);
+router.post('/prototypes/compare-tests/v8/shortcomings', compareTestsController.postShortcomings);*/
 
 // Create route from view path
 router.get('*', miscController.viewFileRoute);
 
 // MOT history vehicle type
-router.post('/prototypes/mot-history-data/cvs/what-vehicle', mothController.postMothType);
+/*router.post('/prototypes/mot-history-data/cvs/what-vehicle', mothController.postMothType);*/
 
 export const allRoutes = router;
