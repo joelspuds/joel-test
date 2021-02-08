16fd55ae4c5c2e7f3d8faf4d60c1588ed6f194a6
Joel Spuds <joel.01@gmail.com>
2021-02-08 08:13:23 +0000

    sorting local links

--- dist/assets/javascripts/gdsOnly.bundle.js
+++ dist/assets/javascripts/gdsOnly.bundle.js
@@ -3,7 +3,7 @@
  *   Name: tfs-front-end/gdsOnly
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: February 7th 2021, 6:58:32 pm
+ *   Timestamp: February 8th 2021, 8:12:47 am
  *   Source: https://github.com/dvsa/front-end
  * 
  */
