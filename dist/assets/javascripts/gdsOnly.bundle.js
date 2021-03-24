f3828dec2ddd6173b1cb2bedf8081ea75783b455
Joel Spuds <joel.01@gmail.com>
2021-03-24 08:46:00 +0000

    embedding Js into header

--- dist/assets/javascripts/gdsOnly.bundle.js
+++ dist/assets/javascripts/gdsOnly.bundle.js
@@ -3,7 +3,7 @@
  *   Name: tfs-front-end/gdsOnly
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: March 24th 2021, 8:39:59 am
+ *   Timestamp: March 24th 2021, 8:45:36 am
  *   Source: https://github.com/dvsa/front-end
  * 
  */
