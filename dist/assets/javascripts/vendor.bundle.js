f957fe96fcedc8941d5e67e0d64abb482b0b2339
Joel Spuds <joel.01@gmail.com>
2021-04-19 08:39:51 +0100

    skip target error fix

--- dist/assets/javascripts/vendor.bundle.js
+++ dist/assets/javascripts/vendor.bundle.js
@@ -3,7 +3,7 @@
  *   Name: tfs-front-end/vendor
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: April 16th 2021, 3:08:12 pm
+ *   Timestamp: April 19th 2021, 8:39:27 am
  *   Source: https://github.com/dvsa/front-end
  * 
  */
