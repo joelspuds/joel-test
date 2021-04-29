cec8897b0304cc992b79f2879f9c1ba2412b71b9
Joel Spuds <joel.01@gmail.com>
2021-04-29 11:44:34 +0100

    gender layout tweaks

--- dist/assets/stylesheets/gdsOnly.css
+++ dist/assets/stylesheets/gdsOnly.css
@@ -3,7 +3,7 @@
  *   Name: tfs-front-end/gdsOnly
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: April 29th 2021, 11:29:49 am
+ *   Timestamp: April 29th 2021, 11:42:16 am
  *   Source: https://github.com/dvsa/front-end
  * 
  */
