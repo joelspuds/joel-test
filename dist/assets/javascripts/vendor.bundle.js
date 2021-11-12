858f61ebab8c63c24bce6b7c4d22ab630912b7d2
Joel Spuds <joel.01@gmail.com>
2021-11-12 15:14:10 +0000

    adding success row for upload

--- dist/assets/javascripts/vendor.bundle.js
+++ dist/assets/javascripts/vendor.bundle.js
@@ -3,7 +3,7 @@
  *   Name: tfs-front-end/vendor
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: November 12th 2021, 2:38:46 pm
+ *   Timestamp: November 12th 2021, 3:05:02 pm
  *   Source: https://github.com/dvsa/front-end
  * 
  */
