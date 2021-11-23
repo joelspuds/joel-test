42fbaee8275012fdb8180f2fbdeb9a8b6005c66b
Joel Spuds <joel.01@gmail.com>
2021-11-23 09:54:57 +0000

    adding stacked banners

--- dist/assets/javascripts/development.bundle.js
+++ dist/assets/javascripts/development.bundle.js
@@ -3,7 +3,7 @@
  *   Name: tfs-front-end/development
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: November 18th 2021, 4:31:56 pm
+ *   Timestamp: November 23rd 2021, 9:49:59 am
  *   Source: https://github.com/dvsa/front-end
  * 
  */
