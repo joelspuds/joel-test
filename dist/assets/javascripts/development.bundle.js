a2764b8fcd4d3be631832bddacbae38852fe6894
Joel Spuds <joel.01@gmail.com>
2022-01-25 12:16:13 +0000

    wide table print fix

--- dist/assets/javascripts/development.bundle.js
+++ dist/assets/javascripts/development.bundle.js
@@ -3,7 +3,7 @@
  *   Name: tfs-front-end/development
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: January 24th 2022, 2:51:44 pm
+ *   Timestamp: January 25th 2022, 12:11:19 pm
  *   Source: https://github.com/dvsa/front-end
  * 
  */
