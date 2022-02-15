b03f4e6ee1030c7276219eba08a9031bb2e57499
Joel Spuds <joel.01@gmail.com>
2022-02-15 15:32:22 +0000

    changing panel user name

--- dist/assets/javascripts/gdsOnly.bundle.js
+++ dist/assets/javascripts/gdsOnly.bundle.js
@@ -3,7 +3,7 @@
  *   Name: tfs-front-end/gdsOnly
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: February 11th 2022, 4:22:16 pm
+ *   Timestamp: February 15th 2022, 3:20:39 pm
  *   Source: https://github.com/dvsa/front-end
  * 
  */
