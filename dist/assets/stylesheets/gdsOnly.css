07bac26f8d199d1e530b274b39cf9478f331754b
Joel Spuds <joel.01@gmail.com>
2022-03-01 16:58:29 +0000

    adding decline invite content tweaks

--- dist/assets/stylesheets/gdsOnly.css
+++ dist/assets/stylesheets/gdsOnly.css
@@ -3,7 +3,7 @@
  *   Name: tfs-front-end/gdsOnly
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: March 1st 2022, 11:11:39 am
+ *   Timestamp: March 1st 2022, 4:50:53 pm
  *   Source: https://github.com/dvsa/front-end
  * 
  */
