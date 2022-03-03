e29e20f8ae40775b8c3e87b026d5194504d49daf
Joel Spuds <joel.01@gmail.com>
2022-03-03 12:09:34 +0000

    panel tweaks

--- dist/assets/stylesheets/development.css
+++ dist/assets/stylesheets/development.css
@@ -3,7 +3,7 @@
  *   Name: tfs-front-end/development
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: March 1st 2022, 4:50:53 pm
+ *   Timestamp: March 3rd 2022, 12:07:18 pm
  *   Source: https://github.com/dvsa/front-end
  * 
  */
