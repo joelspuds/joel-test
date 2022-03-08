4501a458f9ad63205249efa3aa7a73bae7bd1278
Joel Spuds <joel.01@gmail.com>
2022-03-08 09:59:58 +0000

    updatingalerts

--- dist/assets/javascripts/vendor.bundle.js
+++ dist/assets/javascripts/vendor.bundle.js
@@ -3,7 +3,7 @@
  *   Name: tfs-front-end/vendor
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: March 7th 2022, 3:51:05 pm
+ *   Timestamp: March 8th 2022, 9:57:26 am
  *   Source: https://github.com/dvsa/front-end
  * 
  */
