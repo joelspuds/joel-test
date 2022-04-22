113f1326daf3b210cf5fe68d733d74a0f7c90e19
Joel Spuds <joel.01@gmail.com>
2022-04-22 14:24:02 +0100

    updating word wrap on upload items

--- dist/assets/javascripts/gdsOnly.bundle.js
+++ dist/assets/javascripts/gdsOnly.bundle.js
@@ -3,7 +3,7 @@
  *   Name: tfs-front-end/gdsOnly
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: April 8th 2022, 10:31:20 am
+ *   Timestamp: April 22nd 2022, 2:21:23 pm
  *   Source: https://github.com/dvsa/front-end
  * 
  */
