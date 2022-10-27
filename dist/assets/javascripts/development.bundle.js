ec722de6ffade439db4d181f8370c626fe7157e4
Joel Spuds <joel.01@gmail.com>
2022-10-27 09:20:44 +0100

    adding totals column

--- dist/assets/javascripts/development.bundle.js
+++ dist/assets/javascripts/development.bundle.js
@@ -3,7 +3,7 @@
  *   Name: tfs-front-end/development
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: October 26th 2022, 4:17:36 pm
+ *   Timestamp: October 27th 2022, 9:17:08 am
  *   Source: https://github.com/dvsa/front-end
  * 
  */
