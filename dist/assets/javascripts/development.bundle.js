2eb1a013fdbe88fd8e4194ce8718d2cc639f7305
Joel Spuds <joel.01@gmail.com>
2023-02-22 13:36:10 +0000

    novalidate on form

--- dist/assets/javascripts/development.bundle.js
+++ dist/assets/javascripts/development.bundle.js
@@ -3,7 +3,7 @@
  *   Name: tfs-front-end/development
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: February 22nd 2023, 1:13:45 pm
+ *   Timestamp: February 22nd 2023, 1:33:34 pm
  *   Source: https://github.com/dvsa/front-end
  * 
  */
