d3ad9f8147d787bb47ef0693e8eda6b25c11a54b
Joel Spuds <joel.01@gmail.com>
2023-04-13 10:53:51 +0100

    tweaking application list page search and content

--- dist/assets/javascripts/gdsOnly.bundle.js
+++ dist/assets/javascripts/gdsOnly.bundle.js
@@ -3,7 +3,7 @@
  *   Name: tfs-front-end/gdsOnly
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: April 11th 2023, 11:54:16 am
+ *   Timestamp: April 13th 2023, 10:45:51 am
  *   Source: https://github.com/dvsa/front-end
  * 
  */
