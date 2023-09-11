2078f93471f4fd4a42885655de39bc8cfb89877a
Joel Spuds <joel.01@gmail.com>
2023-09-11 10:56:49 +0100

    fixing typo

--- dist/assets/javascripts/vendor.bundle.js
+++ dist/assets/javascripts/vendor.bundle.js
@@ -3,7 +3,7 @@
  *   Name: tfs-front-end/vendor
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: September 8th 2023, 9:34:04 am
+ *   Timestamp: September 11th 2023, 9:52:52 am
  *   Source: https://github.com/dvsa/front-end
  * 
  */
